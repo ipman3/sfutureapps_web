af6723cf18f0ede3ff30c3305b42148db2e5a476
ipman3 <khut.lihong@gmail.com>
2024-05-18 17:22:19 +0700

    chore: Send push issue notification to Telegram with commit details

--- app/api/v3/github/pushNotificationGithub/route.ts
+++ app/api/v3/github/pushNotificationGithub/route.ts
@@ -6,26 +6,42 @@ export async function POST(req: Request) {
   const chatID = -1002028789031; /// Nealika DevOp Team
   //   const chatID = -1002067650868; /// TestingBot group
 
+  /** Get heders */
+  // const headers: any = req.headers;
+  // console.log("headers", headers.get("x-github-event"));
+  // const event = headers.get("x-github-event");
+
   try {
     const ob = await req.json();
-    const message = `New commit%0A
+    if (ob.pusher.name !== "ipman3") {
+      const message = `New commit%0A
     ->Repo: ${ob.repository.name}%0A
     ->Ref: ${ob.ref}%0A
     ->User:  ${ob.pusher.name}%0A
     ->Msg: ${ob.commits[0].message}%0A
     ->Time: ${ob.commits[0].timestamp}`;
 
-    const res_Telegram = await fetch(
-      `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${message}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-    const telData = res_Telegram.json();
-    return NextResponse.json({ status: 1, message: "success", data: telData });
+      const res_Telegram = await fetch(
+        `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${message}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      const telData = res_Telegram.json();
+      return NextResponse.json({
+        status: 1,
+        message: "success",
+        data: telData,
+      });
+    }
+    return NextResponse.json({
+      status: 1,
+      message: "success",
+      data: [],
+    });
   } catch (error: any) {
     return NextResponse.json({ status: 0, message: error.message });
   }
