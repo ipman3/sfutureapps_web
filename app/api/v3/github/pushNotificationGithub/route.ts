0de0be7301a373a68cb0ef0c7887e9482c150226
ipman3 <khut.lihong@gmail.com>
2024-05-18 16:41:42 +0700

    chore: Update Telegram chat ID for push issue notification

--- app/api/v3/github/pushNotificationGithub/route.ts
+++ app/api/v3/github/pushNotificationGithub/route.ts
@@ -3,14 +3,20 @@ export const dynamic = "force-dynamic"; // defaults to auto
 export async function POST(req: Request) {
   // replace the value below with the Telegram token you receive from @BotFather
   const token = "7195173599:AAEBaZpnGk_n7vs_-OuG2fSn6KjlCAn494s";
+  const chatID = -1002028789031; /// Nealika DevOp Team
+  //   const chatID = -1002067650868; /// TestingBot group
 
   try {
     const ob = await req.json();
-    const message = `
-    =>Repo: ${ob.repository.name}%0A=>Event: ${ob.ref}%0A=>User:  ${ob.commits[0].committer.name}%0A=>Msg: ${ob.commits[0].message}%0A=>Time: ${ob.commits[0].timestamp}`;
+    const message = `New commint%0A
+    ->Repo: ${ob.repository.name}%0A
+    ->Ref: ${ob.ref}%0A
+    ->User:  ${ob.pusher.name}%0A
+    ->Msg: ${ob.commits[0].message}%0A
+    ->Time: ${ob.commits[0].timestamp}`;
 
     const res_Telegram = await fetch(
-      `https://api.telegram.org/bot${token}/sendMessage?chat_id=-1002067650868&text=${message}`,
+      `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${message}`,
       {
         method: "POST",
         headers: {
